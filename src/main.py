2a29a639efe86c2f20979f091d4f6ab4d9911bdf
Yomudogly <khalnepesov@gmail.com>
2020-03-30 17:42:09 -0400

    added pagination to /sizes endpoint

--- src/main.py
+++ src/main.py
@@ -36,7 +36,41 @@ CORS(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+### PAGINATION #####
 
+def get_paginated_list(results, url, start, limit):
+    start = int(start)
+    limit = int(limit)
+    count = len(results)
+    if count < start or limit < 0:
+        abort(404)
+    # make response
+    obj = {}
+    obj['start'] = start
+    obj['limit'] = limit
+    obj['count'] = count
+    # make URLs
+    # make previous url
+    if start == 1:
+        obj['previous'] = ''
+    else:
+        start_copy = max(1, start - limit)
+        limit_copy = start - 1
+        obj['previous'] = url + '?start=%d&limit=%d' % (start_copy, limit_copy)
+    # make next url
+    if start + limit > count:
+        obj['next'] = ''
+    else:
+        start_copy = start + limit
+        obj['next'] = url + '?start=%d&limit=%d' % (start_copy, limit)
+    # finally extract result according to bounds
+    obj['results'] = results[(start - 1):(start - 1 + limit)]
+    return obj
+
+
+
+
+#####################
 
 
 ### API SIZES ###
@@ -48,8 +82,11 @@ class AllSizes(Resource):
     def get(self):
         sizes = Sizes_shoes.query.all()
         sizes = list(map(lambda x: x.serialize(), sizes))
-
-        return jsonify(sizes)
+        
+        return jsonify(get_paginated_list(sizes, '/sizes', 
+            start=request.args.get('start', 1), 
+            limit=request.args.get('limit', 20)
+    ))
 
 
 @api.route('/sizes/<int:id>')
